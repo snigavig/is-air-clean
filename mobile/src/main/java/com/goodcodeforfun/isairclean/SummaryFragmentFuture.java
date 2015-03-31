89a03cc6b2fb38ce7ded9067daa04a4b542d1c94
snigavig <snigavig@gmail.com>
2015-03-31 22:55:46 +0300

    data for SummaryFuture + charts improvements

--- mobile/src/main/java/com/goodcodeforfun/isairclean/SummaryFragmentFuture.java
+++ mobile/src/main/java/com/goodcodeforfun/isairclean/SummaryFragmentFuture.java
@@ -2,16 +2,24 @@ package com.goodcodeforfun.isairclean;
 
 import android.app.Activity;
 import android.content.res.Configuration;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.goodcodeforfun.isairclean.data.AirContract;
 
 
 /**
@@ -22,7 +30,7 @@ import android.webkit.WebView;
  * Use the {@link SummaryFragmentFuture#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SummaryFragmentFuture extends Fragment {
+public class SummaryFragmentFuture extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -33,7 +41,34 @@ public class SummaryFragmentFuture extends Fragment {
     private String mParam2;
     private WebView mWebView;
 
+    public TextView carbonFutureView;
+    public TextView energyFutureView;
+    public TextView intensityFutureView;
+    public static String initChartString;
     private OnFragmentInteractionListener mListener;
+
+    private static final int LOADER_ID = 3;
+
+    private Uri mUri;
+
+    private static final String[] SUMMARY_COLUMNS = {
+            AirContract.LocationEntry.TABLE_NAME + "." + AirContract.ObjectEntry.COLUMN_CARBON_FUTURE,
+            AirContract.LocationEntry.TABLE_NAME + "." + AirContract.ObjectEntry.COLUMN_ENERGY_FUTURE,
+            AirContract.LocationEntry.TABLE_NAME + "." + AirContract.ObjectEntry.COLUMN_INTENSITY_FUTURE,
+            AirContract.LocationEntry.COLUMN_FOSSIL_FUTURE,
+            AirContract.LocationEntry.COLUMN_NUCLEAR_FUTURE,
+            AirContract.LocationEntry.COLUMN_HYDRO_FUTURE,
+            AirContract.LocationEntry.COLUMN_RENEWABLE_FUTURE
+
+    };
+
+    private static final int COL_LOCATION_CARBON_FUTURE = 0;
+    private static final int COL_LOCATION_ENERGY_FUTURE = 1;
+    private static final int COL_LOCATION_INTENSITY_FUTURE = 2;
+    private static final int COL_SUMMARY_FOSSIL_FUTURE = 3;
+    private static final int COL_SUMMARY_NUCLEAR_FUTURE = 4;
+    private static final int COL_SUMMARY_HYDRO_FUTURE = 5;
+    private static final int COL_SUMMARY_RENEWABLE_FUTURE = 6;
 
     /**
      * Use this factory method to create a new instance of
@@ -69,13 +104,20 @@ public class SummaryFragmentFuture extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_summary_future, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_summary_future, container, false);
+
+        String locationSetting = Util.getPreferredLocation(getActivity());
+        mUri = AirContract.LocationEntry.buildLocationBySetting(locationSetting);
+        carbonFutureView = (TextView) rootView.findViewById(R.id.summaryFutureCarbonTextView);
+        energyFutureView = (TextView) rootView.findViewById(R.id.summaryFutureEnergyTextView);
+        intensityFutureView = (TextView) rootView.findViewById(R.id.summaryFutureIntensityTextView);
+        return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(LOADER_ID, null, this);
         mWebView = (WebView) getActivity().findViewById(R.id.pieChartFutureWebView);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -109,6 +151,50 @@ public class SummaryFragmentFuture extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if ( mUri != null ) {
+            return new CursorLoader(
+                    getActivity(),
+                    mUri,
+                    SUMMARY_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (!data.moveToFirst()) { return; }
+
+        String carbonFutureString = String.valueOf(data.getFloat(COL_LOCATION_CARBON_FUTURE));
+        String energyFutureString = String.valueOf(data.getFloat(COL_LOCATION_ENERGY_FUTURE));
+        String intensityFutureString = String.valueOf(data.getFloat(COL_LOCATION_INTENSITY_FUTURE));
+        carbonFutureView.setText(carbonFutureString);
+        energyFutureView.setText(energyFutureString);
+        intensityFutureView.setText(intensityFutureString);
+        initChartString = String.format("javascript: window.initChart(%f, %f, %f, %f);",
+                data.getFloat(COL_SUMMARY_FOSSIL_FUTURE)*100,
+                data.getFloat(COL_SUMMARY_NUCLEAR_FUTURE)*100,
+                data.getFloat(COL_SUMMARY_HYDRO_FUTURE)*100,
+                data.getFloat(COL_SUMMARY_RENEWABLE_FUTURE)*100);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                mWebView.loadUrl(initChartString); //
+            }
+        });
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 
     /**
