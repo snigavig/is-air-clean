aa28ef4258b57ec0ec83f3b6aa45bfe45a86b8c6
snigavig <snigavig@gmail.com>
2015-04-04 22:32:18 +0300

    added location updates preference

--- mobile/src/main/java/com/goodcodeforfun/isairclean/IsAirCleanApplication.java
+++ mobile/src/main/java/com/goodcodeforfun/isairclean/IsAirCleanApplication.java
@@ -1,6 +1,8 @@
 package com.goodcodeforfun.isairclean;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.littlefluffytoys.littlefluffylocationlibrary.LocationLibrary;
@@ -10,19 +12,27 @@ import com.littlefluffytoys.littlefluffylocationlibrary.LocationLibrary;
  */
 public class IsAirCleanApplication extends Application {
 
+
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // output debug to LogCat, with tag LittleFluffyLocationLibrary
-        LocationLibrary.showDebugOutput(true);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String locationUpdatesKey = this.getString(R.string.pref_enable_location_updates_key);
+        boolean locationUpdates = prefs.getBoolean(locationUpdatesKey,
+                Boolean.parseBoolean(this.getString(R.string.pref_enable_location_updates_default)));
+        if (locationUpdates) {
+            // output debug to LogCat, with tag LittleFluffyLocationLibrary
+            LocationLibrary.showDebugOutput(true);
 
-        try {
-            // LocationLibrary.initialiseLibrary(getBaseContext(), "com.goodcodeforfun.isairclean");
-            LocationLibrary.initialiseLibrary(getBaseContext(), 60 * 1000, 2 * 60 * 1000, "com.goodcodeforfun.isairclean");
-        }
-        catch (UnsupportedOperationException ex) {
-            Log.d("IsAirClean", "UnsupportedOperationException thrown - the device doesn't have any location providers");
+            try {
+                // LocationLibrary.initialiseLibrary(getBaseContext(), "com.goodcodeforfun.isairclean");
+                LocationLibrary.initialiseLibrary(getBaseContext(), 60 * 1000, 2 * 60 * 1000, "com.goodcodeforfun.isairclean");
+            }
+            catch (UnsupportedOperationException ex) {
+                Log.d("IsAirClean", "UnsupportedOperationException thrown - the device doesn't have any location providers");
+            }
         }
     }
 }
